c096d8514a602251b4686e0d4ab7991d5532caf8
Tim Barbiers <tim.barbiers@gmail.com>
2023-07-09 13:01:51 +0200

    allow for exporting of meta-data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,6 @@
-'use client'
-
-import ThemeSelect, { Theme } from '@/components/theme-select'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { useState } from 'react'
-import Navigation from '@/components/navigation'
-import Header from '@/components/header'
-
-const inter = Inter({ subsets: ['latin'] })
+import LayoutWrap from '@/components/layout-wrap'
 
 export const metadata: Metadata = {
   title: 'Tim Barbiers',
@@ -20,27 +12,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const [theme, setTheme] = useState<Theme>("light")
   
   return (
     <html lang="en">
-        <body className={`${inter.className} ${theme === "dark" ? "dark" : ""}`}>
-          <div className={`flex w-screen h-screen bg-white dark:bg-slate-900 dark:text-white`}>
-            <ThemeSelect 
-              className='absolute -left-10 bottom-24' 
-              selectedTheme={theme} 
-              onChange={setTheme}/>
-            <div className='w-full m-10 px-2 sm:px-10 border-l-2 sm:flex'>
-              <header>
-                <Header className='pb-3'/>
-                <Navigation/>
-              </header>
-              <main className='ml-24 w-1/2 sm:grid sm:content-center'>
-                {children}
-              </main>
-            </div>
-          </div>
-       </body>
+      <LayoutWrap>
+        {children}
+      </LayoutWrap>
     </html>
   )
 }
---
+++ components/layout-wrap.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { Inter } from "next/font/google"
+import Header from "./header"
+import Navigation from "./navigation"
+import ThemeSelect, { Theme } from "./theme-select"
+import { useState } from "react"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function LayoutWrap({
+    children,
+  }: {
+    children: React.ReactNode
+  }): JSX.Element {
+    const [theme, setTheme] = useState<Theme>("light")
+
+    return <body className={`${inter.className} ${theme === "dark" ? "dark" : ""}`}>
+    <div className={`flex w-screen h-screen bg-white dark:bg-slate-900 dark:text-white`}>
+      <ThemeSelect 
+        className='absolute -left-10 bottom-24' 
+        selectedTheme={theme} 
+        onChange={setTheme}/>
+      <div className='w-full m-10 px-2 sm:px-10 border-l-2 sm:flex'>
+        <header>
+          <Header className='pb-3'/>
+          <Navigation/>
+        </header>
+        <main className='ml-24 w-1/2 sm:grid sm:content-center'>
+          {children}
+        </main>
+      </div>
+    </div>
+ </body>
+}
