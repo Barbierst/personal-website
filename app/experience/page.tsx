cc8fec210a3b49789da7507025bbe36b36c10815
Tim Barbiers <tim.barbiers@gmail.com>
2023-08-01 14:51:51 +0200

    onClick expand

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import ExpandButton from "@/components/expand-button"
+import { useState } from "react"
 
 export default function Experience(): JSX.Element {
     const experiences: Experience[] = [
-        {employer: "Sandwave.io", role: "Software Engineer", start: "october 2021", end: "present"},
+        {employer: "Sandwave.io", role: "Software Engineer", start: "october 2021", end: "present", description: "test"},
         {employer: "Maxilia", role: "Software Engineer", start: "november 2021", end: "september 2021"},
         {employer: "Bttr", role: "Software Engineer", start: "september 2019", end: "october 2020"},
         {employer: "Sandd", role: "Marketing Intelligence Analist", start: "febraury 2019", end: "august 2019"},
@@ -29,12 +32,14 @@ function ExperienceBlock({
     end,
     description
 }: Experience): JSX.Element {
+    const [expanded, setExpanded] = useState<boolean>(false)
+
     return <li className="mb-6 pb-4 text-xs border-b-2">
         <p className="text-base">{employer} | {role}</p>
         <span className="flex justify-between">
             <p>{start} - {end}</p>
-            <ExpandButton/>
+            <ExpandButton expanded={expanded} onClick={() => setExpanded(!expanded)}/>
         </span>
-        {description && <p className="text-sm pt-2 text-slate-700 dark:text-gray-400">{description}</p>}
+        {expanded && <p className="text-sm pt-2 text-slate-700 dark:text-gray-400">{description}</p>}
     </li>
 }
--- components/expand-button.tsx
+++ components/expand-button.tsx
@@ -2,15 +2,20 @@
 
 import { useTheme } from "@/modules/theme/theme-context"
 
-export default function ExpandButton(): JSX.Element {
+interface ExpandButtonProps {
+    expanded: boolean
+    onClick: () => void
+}
+
+export default function ExpandButton(props: ExpandButtonProps): JSX.Element {
     const { theme } = useTheme()
+    
+    const fill = theme === "dark" ? "#FFFFFF" : "rgb(30 41 59)" 
 
-    const fill = theme === "dark" ? "#FFFFFF" : "#000000" 
-
-    return <svg className="cursor-pointer" width="15" height="15" viewBox="0 0 70 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+    return <svg onClick={props.onClick} className="cursor-pointer" width="15" height="15" viewBox="0 0 70 71" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="button">
             <rect id="horizontal" y="28" width="70" height="15" rx="7.5" fill={fill}/>
-            <rect id="verticle" x="27.5" y="70.5" width="70" height="15" rx="7.5" transform="rotate(-90 27.5 70.5)" fill={fill}/>
+            <rect id="vertical" x="27.5" y="70.5" width="70" height="15" rx="7.5" transform="rotate(-90 27.5 70.5)" fill={fill}/>
         </g>
     </svg>
 }
