b0758d1027315309d274e335d9cb7ec9ac804746
Tim Barbiers <tim.barbiers@gmail.com>
2023-08-19 20:20:57 +0200

    restyle

--- components/experience-block.tsx
+++ components/experience-block.tsx
@@ -20,14 +20,16 @@ export function ExperienceBlock({
 }: Experience): JSX.Element {
   const [expanded, setExpanded] = useState<boolean>(false)
 
-  return <li className="mb-6 pb-4 text-xs border-b-2">
-    <p className="text-base">{employer} | {role}</p>
-    <span className="flex justify-between">
-      <p>{start} - {end}</p>
-      <ExpandButton expanded={expanded} onClick={() => setExpanded(!expanded)}/>
+  return <li className="mb-6 pb-4 border-b-2">
+    <p>{employer} | {role}</p>
+    <span className="text-sm lg:text-lg">
+      <span className="flex justify-between">
+        <p>{start} - {end}</p>
+        <ExpandButton expanded={expanded} onClick={() => setExpanded(!expanded)}/>
+      </span>
+      <ul className={`list-disc pl-4 transition-all overflow-hidden ${expanded ? "max-h-64": "max-h-0"}`}>
+        {highlights.map((highlight, index) => <li key={index} className="text-stone-700 dark:text-gray-200">{highlight}</li>)}
+      </ul>
     </span>
-    <ul className={`list-disc pl-3 transition-all overflow-hidden ${expanded ? "max-h-48": "max-h-0"}`}>
-      {highlights.map((highlight, index) => <li key={index} className="py-1 text-xs text-stone-700 dark:text-gray-200">{highlight}</li>)}
-    </ul>
   </li>
 }
--- components/layout-wrap.tsx
+++ components/layout-wrap.tsx
@@ -17,13 +17,13 @@ export default function LayoutWrap({
   return <body className={`${inter.className} ${theme === "dark" ? "dark" : ""}`}>
     <div className='transition-colors flex w-screen min-h-screen bg-white dark:bg-stone-900 text-stone-900 dark:text-white'>
       <ThemeSelect className='fixed -left-12 bottom-24 -rotate-90 space-x-4 '/>
-      <div className='w-full mx-10 my-8 pl-4 lg:pl-8 border-l-2 lg:flex'>
-        <header>
+      <div className='w-full mx-10 my-8 pl-4 lg:pl-8 border-l-2'>
+        <header className="lg:absolute">
           <Header className='pb-3'/>
           <Navigation theme={theme}/>
         </header>
-        <main className='mt-12 lg:mt-0 w-full sm:w-3/4 flex lg:justify-center items-center'>
-          <div className="lg:w-1/3">
+        <main className='lg:h-full flex justify-center items-center mt-12 lg:mt-0'>
+          <div className="lg:w-1/3 text-base lg:text-2xl">
             {children}
           </div>
         </main>
